cf2686cc50535e7c735b712d2ab95116075fcb5b
Miłosz Augustyński <maugustynski@ADHSJ.HSW-HSJ.PL>
2018-09-10 14:15:20 +0200

    mapa

--- Program.cs
+++ Program.cs
@@ -30,23 +30,29 @@ class Player
 
 internal class Stage
 {
-    public int Floors { get; private set; }
-    public int Width { get; private set; }
-    public int NbRounds { get; private set; }
+    private readonly int floors;
+    private readonly int width;
     public int ExitFloor { get; private set; }
     public int ExitPos { get; private set; }
     public int NbTotalClones { get; private set; }
-    public int NbExtraElevators { get; private set; }
+    public int NbRounds { get; set; }
+    public int NbExtraElevators { get; set; }
     public Dictionary<int, List<int>> Elevators { get; set; }
-
+    public Char[][] Map {get; set;}
     public Stage()
     {
         string[] inputs = Console.ReadLine().Split(' ');
-        Floors = int.Parse(inputs[0]); // number of floors
-        Width = int.Parse(inputs[1]); // width of the area
+        floors = int.Parse(inputs[0]); // number of floors
+        width = int.Parse(inputs[1]); // width of the area
+        Map = new Char[floors][];
+        for (int i = 0; i < floors; i++)
+        {
+            Map[i] = new String(' ', width).ToCharArray();
+        }
         NbRounds = int.Parse(inputs[2]); // maximum number of rounds
         ExitFloor = int.Parse(inputs[3]); // floor on which the exit is found
         ExitPos = int.Parse(inputs[4]); // position of the exit on its floor
+        Map[ExitFloor][ExitPos] = 'X';
         NbTotalClones = int.Parse(inputs[5]); // number of generated clones
         NbExtraElevators = int.Parse(inputs[6]); // number of additional elevators that you can build
         Elevators = new Dictionary<int, List<int>>();
@@ -57,10 +63,12 @@ internal class Stage
             inputs = Console.ReadLine().Split(' ');
             int elevatorFloor = int.Parse(inputs[0]); // floor on which this elevator is found
             int elevatorPos = int.Parse(inputs[1]); // position of the elevator on its floor
+            Map[elevatorFloor][elevatorPos] = 'E';
             if (Elevators.ContainsKey(elevatorFloor))
                 Elevators[elevatorFloor].Add(elevatorPos);
             else
                 Elevators.Add(elevatorFloor, new List<int>{elevatorPos});
+
         }
     }
 }

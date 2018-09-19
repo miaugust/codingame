edcdc8f97e59e139e45372c922078d0ff5290f09
Miłosz Augustyński <maugustynski@ADHSJ.HSW-HSJ.PL>
2018-09-19 15:02:05 +0200

    regex

--- Program.cs
+++ Program.cs
@@ -4,67 +4,72 @@ using System.IO;
 using System.Text;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Globalization;
+using System.Text.RegularExpressions;
+/**
+* Auto-generated code below aims at helping you parse
+* the standard input according to the problem statement.
+**/
 class Solution
 {
     static void Main(string[] args)
     {
-        string[] inputs = Console.ReadLine().Split(' ');
-        int A = int.Parse(inputs[0]);
-        int B = int.Parse(inputs[1]);
-
-        //Dzielniki A
-        Dictionary<int,int> DividersA  = new Dictionary<int, int>();
-        for (int i = 2; i <= A; i++)
+        int n = int.Parse(Console.ReadLine());
+        Console.Error.WriteLine(n);
+        CultureInfo ci = new CultureInfo("en-us");
+        //pierwsze sprawdzenie
+        int k = n;
+        while(k % 2 == 0)
+            k = k / 2;
+        while(k%5 == 0)
+            k = k / 5;
+        
+        if(k == 1)
         {
-            if(A%i == 0)
-                DividersA.Add(i, 0);
-            
-            while(A%i == 0)
+            string answer = ((double)1/n).ToString("f20", ci);
+            for(int i=answer.Length-1; i>2; i--)
             {
-                DividersA[i]++;
-                A = A / i;
+                if(answer[i] == '0')
+                    answer = answer.Substring(0,i);
+                else
+                    break;
             }
-        }
-
-        Dictionary<int,int> DividersB  = new Dictionary<int, int>();
-        foreach (var key in DividersA.Keys)
-        {
-            DividersB.Add(key, 0);
+            Console.WriteLine(answer);    
+            return;
         }
         
-        // sprawdaznie dla każdej liczby, ile dzielników
-        for (int i = 2; i <= B; i++)
+        int[] x = new int[100];
+        
+        int dzielnik = 10;
+        for (int i = 0; i < x.Length; i++)
         {
-            int j = i;
-            foreach (var key in DividersA.Keys)
-            {
-                while(j%key == 0)
-                {
-                    DividersB[key] ++;
-                    j = j / key;
-                }
-            }
+            x[i] = dzielnik / n;
+            dzielnik = (dzielnik - x[i] * n) * 10;
         }
+        
+        var s = String.Join("", x);
+        string r;
+        TryGetRepeatedDigits(s, out r);
 
-        int answer = 0;
-        bool loop = true;
+        Regex rx = new Regex(r);
+        Match m = rx.Match(s);
+        int idx = m.Index;
 
-        while(loop)
-        {
-            foreach (var key in DividersA.Keys)
-            {
-                DividersB[key] -= DividersA[key];
-                if(DividersB[key] < 0)
-                {
-                    loop = false;
-                    break;
-                }
-            }
-            if(loop)
-                answer++;   
-
-        }
-        Console.WriteLine(answer);
     }
+    public static bool TryGetRepeatedDigits(string digitSequence, out string repeatedDigits)
+{
+    repeatedDigits = null;
+    string pattern = @"^\d*(?<repeat>\d+)\k<repeat>+";
+    //string pattern = @"^\d*(?<repeat>\d+)\k<repeat>+\d*$";
+
+    if (Regex.IsMatch(digitSequence, pattern))
+    {
+        Regex r = new Regex(pattern);
+        repeatedDigits = r.Match(digitSequence).Result("${repeat}");
+
+        return true;
+    }
+    else
+        return false;
+}
 }
